c0f89a06ae772fe4ce155f5f294a3913bf95a155
pmp-p <mail.peny@free.fr>
2022-06-09 02:44:30 +0200

    0.0.9

--- scripts/cpython-build-host.sh
+++ scripts/cpython-build-host.sh
@@ -15,7 +15,7 @@ else
     if [ -f ${PYTHON_FOR_BUILD} ]
     then
         REBUILD=false
-        echo "  * will *RE-USE* PYTHON_FOR_BUILD found at ${PYTHON_FOR_BUILD}"
+        echo "  * will *RE-USE* PYTHON_FOR_BUILD found at ${PYTHON_FOR_BUILD}" 1>&2
     else
         REBUILD=true
     fi
@@ -31,7 +31,7 @@ then
 
 
 #export OPT="$CPOPTS -DNDEBUG -fwrapv"
-    cat $ROOT/src/cpython/Tools/wasm/config.host-wasm32-emscripten <<END
+    cat > $ROOT/src/cpython/Tools/wasm/config.host-wasm32-emscripten <<END
 ac_cv_lib_intl_textdomain=no
 ac_cv_func_bind_textdomain_codeset=no
 END
@@ -45,10 +45,12 @@ END
 
     cat >> pyconfig.h <<END
 #ifdef HAVE_LIBINTL_H
+#warning "HAVE_LIBINTL_H but We do not want to link to libintl"
 #undef HAVE_LIBINTL_H
 #endif
 
 #ifdef WITH_LIBINTL
+#warning "WITH_LIBINTL but We do not want to link to libintl"
 #undef WITH_LIBINTL
 #endif
 
@@ -59,20 +61,20 @@ END
     # Prevent freezing bytecode with a different magic
     rm -f $HOST_PREFIX/bin/python3*
 
-    if which python3.11|grep -q python
+    if command -v python3.11
     then
         echo "
 
+    ===================================================================================
+
+            it's not safe to have a python3.11 in the path :
+                $(command -v python3.11)
+            while in pre-release cycle : _sre.MAGIC / bytecode weird errors etc ...
 
     ===================================================================================
 
-            it's not safe to have a python3.11 in the path while in pre-release cycle
-            _sre.MAGIC / bytecode weird errors etc ...
-
-    ===================================================================================
-
-"
-        sleep 3
+" 1>&2
+        sleep 6
     fi
 
 # OPT="$OPT"
@@ -96,7 +98,7 @@ END
 
     reminder: you need clang libffi-dev and usual cpython requirements.
 ==========================================================================
-    "
+    " 1>&2
         exit 1
     fi
 
@@ -108,7 +110,7 @@ else
 
     *   cpython host already built :
             PYTHON_FOR_BUILD=${PYTHON_FOR_BUILD}
-"
+" 1>&2
 fi
 
 
