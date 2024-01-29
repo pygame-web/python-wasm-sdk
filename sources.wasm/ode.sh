26cff8c0c4b03916ee7b57e37c9e49f8efe27cfd
pmp-p <mail.peny@free.fr>
2024-01-29 19:09:10 +0100

    add ode physics

---
+++ sources.wasm/ode.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+. ${CONFIG:-config}
+
+. scripts/emsdk-fetch.sh
+
+
+if pushd ${ROOT}/src
+then
+    if [ -d ODE-wasm ]
+    then
+        echo -n
+    else
+        git clone --recursive --no-tags --depth 1 --single-branch --branch python-wasm-sdk https://github.com/pygame-web/ODE-wasm
+    fi
+
+    mkdir -p $ROOT/build/ode
+
+    pushd $ROOT/build/ode
+        emcmake cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
+ -DODE_NO_THREADING_INTF=OFF -DODE_NO_BUILTIN_THREADING_IMPL=OFF \
+ -DODE_WITH_DEMOS=OFF ${ROOT}/src/ODE-wasm
+    sed -i 's/#error/\/\/#warning/g' ode/src/config.h
+    emmake make install
+    popd
+
+    popd
+fi
+
