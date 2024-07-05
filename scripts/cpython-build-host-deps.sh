cc43b8784572df8d0a6d33d07b6197e24320dbe4
pmp-p <mail.peny@free.fr>
2024-07-05 08:11:49 +0200

    3.1.63.0bi

--- scripts/cpython-build-host-deps.sh
+++ scripts/cpython-build-host-deps.sh
@@ -12,7 +12,8 @@ $HPIP install --upgrade pip
 
 
 # 3.12 and git deprecated setuptools bundling.
-$HPIP install --upgrade setuptools
+# NO MORE: setuptools is patched for bdist_wheel
+# $HPIP install --upgrade setuptools
 
 
 # probably lot of pip install made in cpython-build-emsdk-prebuilt.sh
