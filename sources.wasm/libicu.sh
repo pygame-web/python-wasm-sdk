eeeccd2c5b27547afa1c86a69d0f57078eda7ca1
pmp-p <mail.peny@free.fr>
2024-07-30 07:59:12 +0200

    fix icu

--- sources.wasm/libicu.sh
+++ sources.wasm/libicu.sh
@@ -10,7 +10,7 @@ then
     echo ok
 else
     wget -c https://github.com/unicode-org/icu/releases/download/release-75-1/icu4c-75_1-src.tgz
-    tar xf icu4c-75_1-src.tgz
+    tar xfz icu4c-75_1-src.tgz
 fi
 
 if [ -f $PREFIX/lib/libicui18n.a ]
@@ -23,11 +23,19 @@ else
     mkdir -p $ROOT/build/libicu
 
     pushd $ROOT/build/libicu
-        emconfigure ../../src/icu/source/configure --prefix=$PREFIX \
+        if emconfigure ../../src/icu/source/configure --prefix=$PREFIX \
          --disable-shared --enable-static \
          --disable-samples --disable-tests --disable-tools \
          --disable-extras --disable-draft
-    emmake make install
+        then
+            emmake make install
+        else
+            echo "
+
+    failed to build ICU
+
+"
+        exit 38
     popd
 fi
 
