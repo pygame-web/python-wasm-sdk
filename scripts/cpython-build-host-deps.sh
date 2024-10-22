927553e9959db2cc3148978d6f0ebac1fbe2631e
pmp-p <mail.peny@free.fr>
2024-10-22 08:39:25 +0200

    fix cmake installed too late for host deps

--- scripts/cpython-build-host-deps.sh
+++ scripts/cpython-build-host-deps.sh
@@ -7,16 +7,20 @@ echo "
 " 1>&2
 
 
-# install and update critical packages.
-$HPIP install --upgrade pip
-
-
-# 3.12 and git deprecated setuptools bundling.
-# NO MORE: setuptools is patched for bdist_wheel
-# $HPIP install --upgrade setuptools
-
 
 # probably lot of pip install made in cpython-build-emsdk-prebuilt.sh
 # should only go here in host python
 
 # TODO
+
+
+
+# install and update critical packages.
+$HPIP install --upgrade pip
+
+# 3.12 and git deprecated setuptools bundling.
+# NO MORE: setuptools is patched for bdist_wheel
+$HPIP install --upgrade setuptools
+
+# install early to build some host tools like w2c2
+$HPIP install --upgrade cmake
