8df6932d0cdc2ff3ce3ae215de3d1b8b89172b40
pmp-p <mail.peny@free.fr>
2025-03-08 05:51:55 +0100

    3.1.61.3bi

--- sources.wasm/bullet3.sh
+++ sources.wasm/bullet3.sh
@@ -12,7 +12,12 @@ then
     then
         echo -n
     else
-        git clone --recursive --no-tags --depth 1 --single-branch --branch master https://github.com/bulletphysics/bullet3
+        git clone --recursive --single-branch --branch master https://github.com/bulletphysics/bullet3
+        pushd bullet3
+
+        # tags/3.25
+        git checkout 2c204c49e56ed15ec5fcfa71d199ab6d6570b3f5
+        popd
     fi
 
     mkdir -p $ROOT/build/bullet3
@@ -24,8 +29,8 @@ then
      -DBUILD_CLSOCKET=NO -DUSE_GRAPHICAL_BENCHMARK=NO \
      -DBUILD_OPENGL3_DEMOS=NO -DBUILD_BULLET2_DEMOS=NO -DBUILD_UNIT_TESTS=NO
     emmake make install
-    popd
+    popd # build/bullet3
 
-    popd
+    popd # src
 fi
 
